08e76d88d4991ba2a8ab2d9a9ed7be22c7b9c4c1
masaki rikitoku <msaki3132@rikima.com>
2025-07-15 22:48:26 +0900

    fix for gcs read/write

--- src/main/scala/com/rikima/cli/WordCount.scala
+++ src/main/scala/com/rikima/cli/WordCount.scala
@@ -1,34 +1,20 @@
 package com.rikima.cli
 
 import com.spotify.scio._
-import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions
 import org.apache.beam.sdk.io.FileSystems
 import org.apache.beam.sdk.io.fs.ResourceId
 
 object WordCount {
   def main(cmdlineArgs: Array[String]): Unit = {
-    val (sc, args) = ContextAndArgs(Array(
-      "--runner=DirectRunner",
-      "--gcpTempLocation=gs://rikitoku-sandbox/tmp",
-      "--stagingLocation=gs://rikitoku-sandbox/staging"
-    ))
-
-    /*
-    sc.optionsAs[DataflowPipelineOptions]
-      .setGcpTempLocation("gs://rikitoku-sandbox/tmp")
-
-    sc.optionsAs[DataflowPipelineOptions]
-      .setStagingLocation("gs://rikitoku-sandbox/staging")
-     */
+    val (sc, args) = ContextAndArgs(//cmdlineArgs)
+      Array(
+        "--runner=DirectRunner",
+        "--tempLocation=gs://rikitoku-sandbox/tmp",
+      ))
 
     val exampleData = "gs://dataflow-samples/shakespeare/kinglear.txt"
-<<<<<<< HEAD
     val input = args.getOrElse("input", exampleData)
-    val output = args.getOrElse("output", "./output") //"gs://rikitoku-sandbox/wc")
-=======
-    val input  = exampleData // args.getOrElse("input", exampleData)
-    val output = "gs://rikitoku-sandbox/wc" //args.getOrElse("output", "gs://rikitoku-sandbox/wc")
->>>>>>> 62f79eb4f6ee5c6270b8d49f7cc79beeab7100b4
+    val output = args.getOrElse("output", "gs://rikitoku-sandbox/wc")
 
     println(s"input ${input}")
     println(s"output: ${output}")
